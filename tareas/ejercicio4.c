9331ae43cea4741561f1b28c2b368a2c7bd8116e
JulioCesarRollano <lito9rollano.v@gmail.com>
2025-04-23 17:51:28 -0400

    subi ejercicio4

--- tareas/ejercicio4.c
+++ tareas/ejercicio4.c
@@ -4,11 +4,11 @@ int main(){
    int num1 = 0;
    int suma=0;
    while (1){
-   printf("ingrese un numero :");
+   printf("ingrese un numero por favor:");
    scanf("%d", &num1);
    if (num1 < 0){
-    printf("Programa Finalizado");
-    printf("La %dsuma es \n",suma);
+    printf("Programa Finalizado...");
+    printf("La suma es %d\n",suma);
    break;
    } else if (num1>0){
       suma+=num1;
