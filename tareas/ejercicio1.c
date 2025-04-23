f56139ff99c6966ebcdc4f23df8393556e83a118
JulioCesarRollano <lito9rollano.v@gmail.com>
2025-04-23 17:44:59 -0400

    subi ejercicio1

---
+++ tareas/ejercicio1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include "funcion.h"
+int main(void){
+    int num;
+   while (1){
+   printf("ingrese un numero: ");
+   scanf("%d", &num);
+     if (num==-1){
+       printf("Programa finalizado");
+       break;
+     }
+     if (parImpar(num)){
+       printf("\n Es Par\n");
+     }else {
+      printf("\nES IMPAR\n");
+     }
+   }
+   return 0;
+}
+
