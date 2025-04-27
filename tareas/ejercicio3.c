70e1a90290e696102dd37c906d7b6d9f18777d35
JulioCesarRollano <lito9rollano.v@gmail.com>
2025-04-26 21:22:29 -0400

    este es mi ejercicio 3

--- tareas/ejercicio3.c
+++ tareas/ejercicio3.c
@@ -7,27 +7,27 @@ typedef enum {
 } semaforo;
 
 int main() {
-    semaforo color = ROJO;  // Inicializamos el semáforo en ROJO
-    int ciclos = 0;  // Contador de ciclos
+    semaforo color = ROJO;  
+    int ciclos = 0;  
 
     while (ciclos < 10) {
-        // Cambiar el color según el estado actual
+        
         switch (color) {
             case ROJO:
                 printf("Color ROJO\n");
-                color = VERDE;  // Cambiamos el estado a VERDE
+                color = VERDE;  
                 break;
             case VERDE:
                 printf("Color VERDE\n");
-                color = AMARILLO;  // Cambiamos el estado a AMARILLO
+                color = AMARILLO;  
                 break;
             case AMARILLO:
                 printf("Color AMARILLO\n");
-                color = ROJO;  // Cambiamos el estado a ROJO
+                color = ROJO;  
                 break;
         }
 
-        // Incrementamos el contador de ciclos
+        
         ciclos++;
         printf("Fin del ciclo %d\n", ciclos);
     }
