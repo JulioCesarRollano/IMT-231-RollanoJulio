95ab950a157ffa332f78ce658eb6fb987327d0be
JulioCesarRollano <lito9rollano.v@gmail.com>
2025-04-26 21:29:13 -0400

    este es mi ejercicio 4

---
+++ tareas/ejercicio4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include  "funcion.h"
+int main(){
+   int num1 = 0;
+   int suma=0;
+   while (1){
+   printf("ingrese un numero por favor:");
+   scanf("%d", &num1);
+   if (num1 < 0){
+    printf("Programa Finalizado...");
+    printf("La suma es %d\n",suma);
+   break;
+   } else if (num1>0){
+      suma+=num1;
+    }
+   }
+}
