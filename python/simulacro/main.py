ac6be61bb2350d59f5bd5cf33459f489827c6676
julioCesarRollano <lito9rollano.v@gmail.com>
2025-05-21 21:16:29 -0400

    opcion1 terminada

--- python/simulacro/main.py
+++ python/simulacro/main.py
@@ -1,18 +1,21 @@
 from operaciones import contar
 while True:
-    print("----Menu de opciones----")
-    print("1.Contar cuántos números entre 1 y N son divisibles por 3 **o** " \
-    "terminanen 3")
-    print("2.Imprimir una piramide de asteriscos de altura N")
-    print("3.Dado un número N, mostrar " \
-    "la secuencia de los primeros N términos de la serie: 1, -2, 3, -4, 5, -6,")
-    print("4.Mostrar la cantidad de números primos entre 1 y N.")
-    print("5.Verificar si un número es palíndromo (número capicúa).")
-    print("6.Salir del programa.")
-    opcion=int(input("Selecione una opcion : "))
+    print("-----MENU DE OPCIONES----")
+    print("1. Contar cuántos números entre 1 y N son divisibles por 3")
+    print("2. Imprimir una piramide de asteriscos de altura N.")
+    print("3. Dado un número N, mostrar la secuencia de los primeros N términos " \
+    "de la serie: 1, -2, 3, -4, 5, -6, ")
+    print("4. Mostrar la cantidad de números primos entre 1 y N.")
+    print("5. Verificar si un número es palíndromo (número capicúa).")
+    print("6.Salir del programa. ")
+    opcion =int(input("Seleccione una opciion: "))
     if opcion == 6:
         print("Saliste del programa ")
         break
     elif opcion == 1:
         N=int(input("Ingrese un numero:"))
-        resultado = contar(N)
+        resultado=contar(N)
+        print("Los numeros entre 1 y N son:",resultado,"")
+    else:
+        print("Opcion no valida")
+        
--- python/simulacro/operaciones/funcion1.py
+++ python/simulacro/operaciones/funcion1.py
@@ -1,6 +1,8 @@
 def contar(N):
-    numero=0
-    for i in range (1,N+1,+1):
-        if (i % 3==0):
-            numero+=1
-    print("Los numeros entre 1 y N son:",numero,"")            
+    contador = 0
+    num=0
+    for i in range(1,N+1,1):
+        if i % 3 ==0 or i % 10==3:
+             contador+= 1
+            
+    return contador
